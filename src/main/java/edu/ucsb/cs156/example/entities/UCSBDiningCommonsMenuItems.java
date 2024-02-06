bd04992c16d6ca41671d5ce1892f21a054304e25
Kenisha Vaswani <kenishavaswani@Kenishas-MacBook-Pro.local>
2024-02-06 12:10:50 -0800

    added database table

--- src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItems.java
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItems.java
@@ -14,17 +14,20 @@ import lombok.Builder;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity(name = "ucsbdiningcommons")
-public class UCSBDiningCommons {
+@Entity(name = "ucsbdiningcommonsmenuitems")
+public class UCSBDiningCommonsMenuItems {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
-  private String code;
+  private String diningCommonsCode;
   private String name;
   private String station;
-  private boolean hasSackMeal;
-  private boolean hasTakeOutMeal;
-  private boolean hasDiningCam;
-  private Double latitude;
-  private Double longitude;
+  // private String code;
+  // private String name;
+  // private String station;
+  // private boolean hasSackMeal;
+  // private boolean hasTakeOutMeal;
+  // private boolean hasDiningCam;
+  // private Double latitude;
+  // private Double longitude;
 }
