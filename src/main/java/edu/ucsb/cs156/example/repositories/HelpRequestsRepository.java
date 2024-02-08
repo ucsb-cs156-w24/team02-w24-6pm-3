e766e3a0c51d8235b5bc4ff230bcc2b3e58a34d9
Jennie Wu <86743865+Jenniebn@users.noreply.github.com>
2024-02-07 18:41:37 -0800

    Update HelpRequestsRepository.java

--- src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestsRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestsRepository.java
@@ -1,6 +1,6 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.UCSBDate;
+import edu.ucsb.cs156.example.entities.HelpRequest;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
